5f9a0efc36e0e910e5199bb1f0f1cffd959e30bf
Jan Neduchal <honza.neduchal@gmail.com>
2018-05-08 00:41:02 +0200

    updated comment

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,8 +1,6 @@
 # written by Jan "Duchy" Neduchal
 # 2018
 # Licenced under the MIT licence
-# Do what the f**k ever you want with the code, I don't really care. I <3 opensource.
-# However note, that I, Jan Neduchal, take no responsibility for any malicious of your actions with this code
 from os.path import dirname, basename, isfile
 import glob
 
